edd167a4e0739d4022175c84a7c5bf49af512502
safebuster2 <safebuster2@gmail.com>
2022-05-10 09:49:44 +0200

    порефакторил

--- js/script.js
+++ js/script.js
@@ -1,28 +1,13 @@
-async function showMenu(){
+async function showMenu() {
     let rawMenu = document.getElementById('raw_menu_text').value;
-    if(rawMenu === null || rawMenu.trim() === '') {
-        try {
-            const permission = await navigator.permissions.query({name: 'clipboard-read'});
-            if (permission.state === 'denied') {
-                throw new Error('Not allowed to read clipboard.');
-            }
-            const clipboardContents = await navigator.clipboard.read();
-            for (const item of clipboardContents) {
-                if (!item.types.includes('text/plain')) {
-                    throw new Error('Clipboard contains non-text data: ' + item.types);
-                }
-                const blob = await item.getType('text/plain');
-                rawMenu = await blob.text();
-            }
-        } catch (error) {
-            console.error(error.message);
-        }
+    if (rawMenu === null || rawMenu.trim() === '') {
+        rawMenu = await getTextFromClipboard();
     }
-    if(rawMenu === null || rawMenu.trim() === '') {
+    if (rawMenu === null || rawMenu.trim() === '') {
         return;
     }
     const checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
-    if(checkboxes.length > 0 && !confirm("Сбросить заказ?")) {
+    if (checkboxes.length > 0 && !confirm("Сбросить заказ?")) {
         return;
     }
     const menu = document.getElementById('menu');
@@ -40,15 +25,15 @@ async function showMenu(){
         order.removeChild(order.lastChild);
     }
 
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i].includes("рсд")){
-            var cdId = 'item_' + i;
-            var cb = document.createElement("input");
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].includes("рсд")) {
+            const cdId = 'item_' + i;
+            const cb = document.createElement("input");
             cb.type = "checkbox";
             cb.onclick = updateTotal;
             cb.id = cdId;
 
-            var label = document.createElement("label");
+            const label = document.createElement("label");
             label.setAttribute("for", cdId);
             label.innerText = arr[i];
             label.onclick = updateTotal;
@@ -58,20 +43,20 @@ async function showMenu(){
             menu.appendChild(label);
             menu.appendChild(document.createElement("br"));
         } else {
-            var label = document.createElement("label");
-            label.innerText = arr[i];
+            const fillerLabel = document.createElement("label");
+            fillerLabel.innerText = arr[i];
 
-            var cb = document.createElement("input");
-            cb.type = "checkbox";
-            cb.disabled = true;
+            const fillerCb = document.createElement("input");
+            fillerCb.type = "checkbox";
+            fillerCb.disabled = true;
 
-            menu.appendChild(cb);
-            menu.appendChild(label);
+            menu.appendChild(fillerCb);
+            menu.appendChild(fillerLabel);
             menu.appendChild(document.createElement("br"));
         }
     }
-    if(arr.length > 0){
-        var total = document.createElement("label");
+    if (arr.length > 0) {
+        const total = document.createElement("label");
         total.id = "total";
         total.innerText = "Итого: 0 рсд";
         total.style = "font-weight: bold;";
@@ -80,7 +65,7 @@ async function showMenu(){
         menu.appendChild(total);
         menu.appendChild(document.createElement("br"));
 
-        var submit = document.createElement("input");
+        const submit = document.createElement("input");
         submit.type = "submit";
         submit.id = "copy_order";
         submit.onclick = copyOrderToClipboard;
@@ -91,45 +76,66 @@ async function showMenu(){
     }
 }
 
-function updateTotal(){
+async function getTextFromClipboard() {
+    let text = '';
+    try {
+        const permission = await navigator.permissions.query({name: 'clipboard-read'});
+        if (permission.state === 'denied') {
+            throw new Error('Not allowed to read clipboard.');
+        }
+        const clipboardContents = await navigator.clipboard.read();
+        for (const item of clipboardContents) {
+            if (!item.types.includes('text/plain')) {
+                throw new Error('Clipboard contains non-text data: ' + item.types);
+            }
+            const blob = await item.getType('text/plain');
+            text = await blob.text();
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+    return text;
+}
+
+function updateTotal() {
     const order = document.getElementById('order');
     while (order.hasChildNodes()) {
         order.removeChild(order.lastChild);
     }
-    var sum = 0;
-    var checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
+    let sum = 0;
+    const checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
 
-    for (var i = 0; i < checkboxes.length; i++) {
-        var cbLabel = document.getElementById('label_' + checkboxes[i].id);
+    for (let i = 0; i < checkboxes.length; i++) {
+        const cbLabel = document.getElementById('label_' + checkboxes[i].id);
 
-        var label = document.createElement("label");
+        const label = document.createElement("label");
         label.innerText = cbLabel.innerText;
 
         order.appendChild(label);
         order.appendChild(document.createElement("br"));
 
-        var priceArr = cbLabel.innerText.match(/\s+(\d+)\s+рсд/i);
-        var price = parseInt(priceArr[1]);
+        const priceArr = cbLabel.innerText.match(/\s+(\d+)\s+рсд/i);
+        const price = parseInt(priceArr[1]);
         sum += price;
     }
     const total = document.getElementById('total');
     total.innerText = 'Итого: ' + sum + ' рсд';
 }
 
-function copyOrderToClipboard(){
+function copyOrderToClipboard() {
     const order = document.getElementById('order');
-    var text = order.innerText;
+    const text = order.innerText;
     console.log(text);
-    navigator.clipboard.writeText(text).then(function() {
-      console.log('Async: Copying to clipboard was successful!');
-      notifyCopy('Заказ скопирован');
-    }, function(err) {
-      console.error('Async: Could not copy text: ', err);
-      notifyCopy(err);
+    navigator.clipboard.writeText(text).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+        notifyCopy('Заказ скопирован');
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+        notifyCopy(err);
     });
 }
 
-function notifyCopy(msg){
+function notifyCopy(msg) {
     const btn = document.getElementById('copy_order');
     const tmp = btn.value;
     btn.disabled = true;
@@ -137,5 +143,5 @@ function notifyCopy(msg){
     setTimeout(() => {
         btn.value = tmp;
         btn.disabled = false;
-    }, "1000")
+    }, 1000);
 }
